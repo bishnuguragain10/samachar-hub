b7cc5ee3dac62006a90925277141ad1eaa7d4194
Bishnu <bishnu.guragain.10@gmail.com>
2026-05-06 21:53:16 -0700

    fix missing useLocation import

--- client/src/pages/HiddenAdminAccessPage.tsx
+++ client/src/pages/HiddenAdminAccessPage.tsx
@@ -1,4 +1,5 @@
 import { useState, type FormEvent } from "react";
+import { useLocation } from "wouter";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
